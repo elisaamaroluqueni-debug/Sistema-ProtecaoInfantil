41afc4c0a5d3c8e0b66868ab602e4f3b8e87b47e
Diamonica26 <diamonicacarolinadiatilodasilv@gmail.com>
2026-06-23 14:29:06 +0100

    Update Historico.c
    Coloquei a funcao adicionar historico

--- Historico.c
+++ Historico.c
@@ -2,7 +2,47 @@
 #include <stdlib.h>
 #include "Sistema.h"
 
-//Fun��o para Imprimir Historico
+//função Adicionar Historico 
+void AdicionarHistorico(Historico **lista, int id_denuncia,
+                        const char *anterior,
+                        const char *atual,
+                        const char *data)
+{
+    Historico *novo = (Historico*)malloc(sizeof(Historico));
+
+    if(novo == NULL)
+    {
+        printf("Erro ao alocar memoria para historico!\n");
+        return;
+    }
+
+    novo->id_denuncia = id_denuncia;
+
+    strcpy(novo->estado_anterior, anterior);
+    strcpy(novo->estado_atual, atual);
+    strcpy(novo->data, data);
+
+    novo->prox = NULL;
+
+    if(*lista == NULL)
+    {
+        *lista = novo;
+    }
+    else
+    {
+        Historico *atual_h = *lista;
+
+        while(atual_h->prox != NULL)
+        {
+            atual_h = atual_h->prox;
+        }
+
+        atual_h->prox = novo;
+    }
+}
+
+
+//Função para Imprimir Historico
 void ImprimirHistorico(Historico *lista, int id_denuncia){
 	Historico *atual = lista;
 	int encontrou = 0;
